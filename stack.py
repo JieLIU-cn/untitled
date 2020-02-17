ee13428b3b1dea67bcaba800439715dfa4bdcf9f
JieLIU-cn <18701068821@163.com>
2020-02-17 23:50:09 +0800

    增加了主函数

--- stack.py
+++ stack.py
@@ -1,8 +1,8 @@
 class Stack(object):
-    def __init__(self, limit = 10):
+    def __init__(self, limit=10):
         """
         定义栈为数组
-        定义栈最大空间
+        定义栈最大空间, 默认10
         """
 
         self.stack = []
@@ -39,4 +39,26 @@ class Stack(object):
         return len(self.stack)
 
 
+def is_balanced_parentheses(parentheses):
+    """
+    判断字符串中括号是否成对
+    :param parentheses:  含括号的字符串
+    :return: 真值
+    """
+    stack = Stack(len(parentheses))
+    for i in parentheses:
+        if i == '(':
+            stack.push(i)
+        elif i == ')':
+            if stack.is_empty():
+                return False
+            else:
+                stack.pop()
+    return stack.is_empty()
 
+
+if __name__ == '__main__':
+    examples = ['((()))', '((())', '(()))']
+    print('Balanced parentheses demonstration:\n')
+    for example in examples:
+        print('{}:{}'.format(example, str(is_balanced_parentheses(example))))
