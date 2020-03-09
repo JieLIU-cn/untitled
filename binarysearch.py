e80f29ee0a36fe885573400adf45f532a514ba83
JieLIU-cn <18701068821@163.com>
2020-03-09 23:02:12 +0800

    二分查找

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+def binarysearch(sorted_sequence, target):
+    left = 0
+    right = len(sorted_sequence) - 1
+    while left < right:
+        midpoint = (left + right) // 2
+        if sorted_sequence[midpoint] == target:
+            return midpoint
+        elif sorted_sequence[midpoint] > target:
+            right = midpoint-1
+        elif sorted_sequence[midpoint] < target:
+            left = midpoint+1
+    return None  # 循环到最后也没匹配
+
+
+if __name__ == "__main__":
+    sequence = [1,2,3,4,5,6,7,8,9,10]
+    target = 6
+    print(binarysearch(sequence, target))
